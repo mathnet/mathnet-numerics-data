2649cbc2b8856c584209b2de2b2e7ae5f1bc9915
Christoph Ruegg <git@cdrnet.ch>
2013-09-23 21:10:31 +0200

    Tests: do not use verbatim strings in source (unreliable)

--- src/UnitTests/Text/MatrixMarketWriterTests.cs
+++ src/UnitTests/Text/MatrixMarketWriterTests.cs
@@ -28,6 +28,7 @@
 // OTHER DEALINGS IN THE SOFTWARE.
 // </copyright>
 
+using System;
 using System.IO;
 using System.Numerics;
 using MathNet.Numerics.Data.Text;
@@ -42,50 +43,30 @@ namespace MathNet.Numerics.Data.UnitTests.Text
         public void CanWriteRealDenseMatrix()
         {
             var m = LinearAlgebra.Double.DenseMatrix.OfArray(new[,] {{1.5d, 2}, {3, 4}, {5, 6}});
-            const string expected = @"%%MatrixMarket matrix array real general
-3 2
-1.5
-3
-5
-2
-4
-6
-";
+            const string expected = "%%MatrixMarket matrix array real general\n3 2\n1.5\n3\n5\n2\n4\n6\n";
             var writer = new StringWriter();
             MatrixMarketWriter.WriteMatrix(writer, m);
-            Assert.AreEqual(expected, writer.ToString());
+            Assert.AreEqual(expected.Replace("\n",Environment.NewLine), writer.ToString());
         }
 
         [Test]
         public void CanWriteComplexDenseMatrix()
         {
             var m = LinearAlgebra.Complex.DenseMatrix.OfArray(new[,] { { new Complex(1.5, 0), new Complex(0, 1) }, { new Complex(4, 5), new Complex(6, 7) } });
-            const string expected = @"%%MatrixMarket matrix array complex general
-2 2
-1.5 0
-4 5
-0 1
-6 7
-";
+            const string expected = "%%MatrixMarket matrix array complex general\n2 2\n1.5 0\n4 5\n0 1\n6 7\n";
             var writer = new StringWriter();
             MatrixMarketWriter.WriteMatrix(writer, m);
-            Assert.AreEqual(expected, writer.ToString());
+            Assert.AreEqual(expected.Replace("\n", Environment.NewLine), writer.ToString());
         }
 
         [Test]
         public void CanWriteRealSparseMatrix()
         {
             var m = LinearAlgebra.Double.SparseMatrix.OfArray(new[,] { { 1.5d, 2 }, { 0, 4 }, { 5, 0 } });
-            const string expected = @"%%MatrixMarket matrix coordinate real general
-3 2 4
-1 1 1.5
-1 2 2
-2 2 4
-3 1 5
-";
+            const string expected = "%%MatrixMarket matrix coordinate real general\n3 2 4\n1 1 1.5\n1 2 2\n2 2 4\n3 1 5\n";
             var writer = new StringWriter();
             MatrixMarketWriter.WriteMatrix(writer, m);
-            Assert.AreEqual(expected, writer.ToString());
+            Assert.AreEqual(expected.Replace("\n", Environment.NewLine), writer.ToString());
         }
     }
 }
